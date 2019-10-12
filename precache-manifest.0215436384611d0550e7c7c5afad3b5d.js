41d242937becbb90e9b41969be035f7b0359816d
sdfsfd <sdfd@sdfsd.ff>
2019-10-12 21:33:41 +0300

    Updates

---
+++ precache-manifest.0215436384611d0550e7c7c5afad3b5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90e2abbd67812703dfd35ab6e0827fae",
+    "url": "/GA-RealtimeReport-PWA/index.html"
+  },
+  {
+    "revision": "427114a3edd7acd5bc65",
+    "url": "/GA-RealtimeReport-PWA/static/css/main.8b687e4b.chunk.css"
+  },
+  {
+    "revision": "bda98faa370b568efaf0",
+    "url": "/GA-RealtimeReport-PWA/static/js/2.f2d72b11.chunk.js"
+  },
+  {
+    "revision": "427114a3edd7acd5bc65",
+    "url": "/GA-RealtimeReport-PWA/static/js/main.39517425.chunk.js"
+  },
+  {
+    "revision": "b46306128e71a02e3643",
+    "url": "/GA-RealtimeReport-PWA/static/js/runtime~main.844f3ebc.js"
+  },
+  {
+    "revision": "285fa1532622f111525630af1b110ee5",
+    "url": "/GA-RealtimeReport-PWA/static/media/bg.285fa153.png"
+  }
+]);
